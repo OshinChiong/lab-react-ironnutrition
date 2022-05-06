1d1e15115d09218d3321855a8517f55c784c2dd8
Oshin Chiong <oshinchiong@Oshins-MacBook-Air.local>
2022-05-06 13:55:37 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
+import FoodForm from './components/FoodForm';
+import CalculateCalories from './components/CalculateCalories';
 
 function App() {
+  const [foodArr, setFoodArr] = React.useState(foods);
+
+  function updateQuantity(food) {
+    let updatedArr = foodArr.map((singlefood) => {
+      if (singlefood.name === food.name) {
+        singlefood.quantity++;
+      }
+      return singlefood;
+    });
+    setFoodArr(updatedArr);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Add new Food</h3>
+      <FoodForm setFoodArr={setFoodArr} foodArr={foodArr} />
+
+      <CalculateCalories foodArr={foodArr} />
+
+      {foodArr.map((individualFood) => {
+        return (
+          <FoodBox food={individualFood} updateQuantity={updateQuantity} />
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/CalculateCalories.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+let total;
+const CalculateCalories = (props) => {
+  //This is an array of foods with quantity
+  let selectedFoods = props.foodArr.filter((food) => {
+    return food.quantity > 0;
+  });
+
+  return (
+    <div>
+      <h4>Today's Foods:</h4>
+      <ul>
+        {selectedFoods.map((food) => {
+          total += food.calories * food.quantity;
+          return (
+            <li>
+              {food.quantity} {food.name} {food.calories * food.quantity}
+            </li>
+          );
+        })}
+      </ul>
+      <p>Total calories: {total}</p>
+      <p>
+        Total calories:{' '}
+        {selectedFoods.reduce(
+          (sum, nextVal) => sum + nextVal.quantity * nextVal.calories,
+          0
+        )}
+      </p>
+    </div>
+  );
+};
+
+export default CalculateCalories;
